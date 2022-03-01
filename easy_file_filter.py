dacca2de69ba7fb83b749d65628dce659ea87cee
Noe <berdoznoe@gmail.com>
2022-03-01 23:40:39 +0100

    Move filtered files

--- easy_file_filter.py
+++ easy_file_filter.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 import os
 import re
+import shutil
 
 
 sg.theme('DarkAmber')  # Add a touch of color
@@ -10,7 +11,7 @@ layout = [  [sg.Text('Folder location:')],
             [sg.In(enable_events=True, key="-FOLDER-"), sg.FolderBrowse()],  # Take as input the chosen folder absolute path
             [sg.Listbox(values=[], size=(30, 6), key='-FILES-')],
             [sg.Text('Name to filter:'), sg.InputText(key="-NAME-"), sg.Button('Filter')],
-            [sg.Text('Files filtered:')],
+            [sg.Text(key="-NEW_FOLDER-")],
             [sg.Listbox(values=[], size=(30, 6), key="-MATCHES-")],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
@@ -48,7 +49,16 @@ while True:
             if match:
                 matches_files.append(file)
 
-            window['-MATCHES-'].update(matches_files)  # Show matches files from filter
+        filtered_files_path = values['-FOLDER-'] + '/' + values['-NAME-']
+
+        os.mkdir(filtered_files_path)
+
+        for matche_file in matches_files:
+            shutil.move(values['-FOLDER-'] + '/' + matche_file, filtered_files_path)
+            print('Moved file to /' + values['-NAME-'])
+
+        window['-MATCHES-'].update(matches_files)  # Show matches files from filter
+        window['-NEW_FOLDER-'].update('[!] Moved these files to /' + values['-NAME-'])
 
 window.close()
 
